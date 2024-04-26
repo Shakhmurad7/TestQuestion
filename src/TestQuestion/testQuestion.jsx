3f8c92b41af5067b00313b6b478a29756b027d1d
Shakhmurad <muradb356@gmail.com>
2024-04-27 01:52:03 +0400

    add update in question test

--- src/TestQuestion/testQuestion.jsx
+++ src/TestQuestion/testQuestion.jsx
@@ -1,70 +1,145 @@
+// import React, { useState } from 'react'
+
+// const questions =[
+//     {
+//         title:'React nedi?',
+//         variants: ['Kitabxana' ,'Framevork' , 'tətbiq'],
+//         correct:0
+//     },
+//     {
+//         title:'JS - necenci ilde yaranib',
+//         variants: ['1992' ,'1995' , '1999'],
+//         correct:1
+//     },
+//     {
+//         title:'Hansi proqramlasdirma dilidi',
+//         variants: ['JavaScript' ,'CSS' , 'HTML'],
+//         correct:0
+//     }
+// ]
+
+
+// const Game = ({question , OnClickVariants})=>{
+//     return(
+//         <>
+//             <div className="game-container">
+//                          <h2>{question.title}</h2>
+//                         <div className="game-li">
+//                             {question.variants.map((item , index)=>(
+//                                 <li onClick={()=>OnClickVariants(index)} key={item}>{item}</li>
+//                             ))}
+//                         </div>
+//             </div>
+//         </>
+//     )
+// }
+
+// const Result = ({correct})=>{
+//     return(
+//         <>
+//             <div className="game-container">
+//                 <h2>siz {correct}/{questions.length} tapdiz</h2>
+//                 <h3>
+//                     <a href={'/'}>Yeniden Basla</a></h3>
+//             </div>
+//         </>
+//     )
+// }
+
+
+// function TestQuestion() {
+//     const [step , setStep] = useState(0)
+//     const [correct , setCorrect] = useState(0)
+//     const question = questions[step]
+//     const OnClickVariants = (index)=>{
+//         console.log(step , index);
+//         setStep(step + 1)
+
+//         if(index === question.correct){
+//             setCorrect(correct + 1)
+//         }
+//     }
+//   return (
+//     <>  
+//     {
+//         step !== questions.length ?  <Game question={question} OnClickVariants={OnClickVariants} /> : <Result correct={correct} />
+//     }
+    
+//     </>
+//   )
+// }
+
+// export default TestQuestion
+
 import React, { useState } from 'react'
 
-const questions =[
-    {
-        title:'React nedi?',
-        variants: ['Kitabxana' ,'Framevork' , 'tətbiq'],
-        correct:0
-    },
-    {
-        title:'JS - necenci ilde yaranib',
-        variants: ['1992' ,'1995' , '1999'],
-        correct:1
-    },
-    {
-        title:'Hansi proqramlasdirma dilidi',
-        variants: ['JavaScript' ,'CSS' , 'HTML'],
-        correct:0
-    }
+const questions = [
+            { 
+                title:'React nedi?',
+                variants: ['Kitabxana' ,'Framevork' , 'tətbiq'],
+                correct:0
+            },
+            {
+                title:'JS - necenci ilde yaranib',
+                variants: ['1992' ,'1995' , '1999'],
+                correct:1
+            },
+            {
+                title:'Hansi proqramlasdirma dilidi',
+                variants: ['JavaScript' ,'CSS' , 'HTML'],
+                correct:0
+            }
 ]
 
 
-const Game = ({question , OnClickVariants})=>{
+const Game = ({question , onClickVariants , size})=>{
     return(
         <>
             <div className="game-container">
-                         <h2>{question.title}</h2>
-                        <div className="game-li">
-                            {question.variants.map((item , index)=>(
-                                <li onClick={()=>OnClickVariants(index)} key={item}>{item}</li>
-                            ))}
-                        </div>
+                <div style={{width:`${size}%` ,transition:'.4s' ,  backgroundColor:'white' , height:'7px' , borderRadius:'10px'}}></div>
+                <h2>{question.title}</h2>
+                <div className="game-li">
+                    {
+                        question.variants.map((item , index)=>(
+                                <li key={index} onClick={()=>onClickVariants(index)} >{item}</li>
+                        ))
+                    }
+                </div>
             </div>
         </>
     )
 }
 
-const Result = ({correct})=>{
+const Reuslt = ({correct })=>{
     return(
         <>
-            <div className="game-container">
-                <h2>siz {correct}/{questions.length} tapdiz</h2>
-                <h3>
-                    <a href={'/'}>Yeniden Basla</a></h3>
-            </div>
+           <div className="game-container">
+                    <h2>siz {correct}/{questions.length} tapdiz</h2><h3>
+                     <a href={'/'}>Yeniden Basla</a></h3>
+        </div>
         </>
     )
 }
-
 
 function TestQuestion() {
     const [step , setStep] = useState(0)
-    const [correct , setCorrect] = useState(0)
+    const [correct , setCorrect ] = useState(0)
     const question = questions[step]
-    const OnClickVariants = (index)=>{
-        console.log(step , index);
+    const onClickVariants = (index)=>{
         setStep(step + 1)
 
         if(index === question.correct){
-            setCorrect(correct + 1)
+            setCorrect(correct+1)
         }
     }
+
+    const size = Math.round((step / questions.length) * 100)
+    console.log(size);
   return (
-    <>  
+    <>
     {
-        step !== questions.length ?  <Game question={question} OnClickVariants={OnClickVariants} /> : <Result correct={correct} />
+        step !== questions.length ? <Game question={question} size={size} onClickVariants ={onClickVariants}/> : <Reuslt correct={correct} />
     }
-    
     </>
   )
 }
